00284f1d27d7571fa7d300a530bc9af00f0c7a89
css <seop7379@daum.net>
2023-01-19 10:39:21 +0900

    카드 뉴스 2일차

---
+++ README.md
@@ -0,0 +1,5 @@
+# 2023 01 19
+
+## 파이썬 mongoDB object id 관련 문제 해결
+
+## 바닐라 스크립트 append나 innerHTML이 아닌 insertAdjacentHTML를 통한 렌더링 속도향상
--- app.py
+++ app.py
@@ -2,25 +2,53 @@ import requests;
 import json;
 from flask import Flask,render_template,request,jsonify
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 with open('config.json', 'r') as f:
     config = json.load(f)        
     conn=MongoClient(config["MONGO"]["url"]);
     db=conn.PythonDB;
 #영화정보 가져오기    
-def findMovieInfo(movie_url):
-    print(movie_url);   
+def findMovieInfo(params):
+    movie_url = params["url"];
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(movie_url,headers=headers)     
+    # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+    # soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+    # 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+    # 영화 정보를 통해 가져옴 => naver가 아닐 경우에 대하여 에러처리를 해줘야한다.
+    soup = BeautifulSoup(data.text, 'html.parser')
+    #print(soup)  # HTML을 받아온 것을 확인할 수 있다.
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    url_image = og_image['content']
+    url_title = og_title['content']
+    url_description = og_description['content']
+    movieObj = {}
+    if url_image is not None:
+        movieObj = {"movie_url" : movie_url,"url_image" : url_image,"url_title":url_title,"url_description":url_description,"comment" : params["comment"]};
+        db.movieInfo.insert_one(movieObj)
+    return db.movieInfo.find_one({"url_title":url_title});
+
+    
 
 @app.route('/')
 def home():
    return render_template('index.html');
 
 #데이터 호출
-@app.route("/movie",methods=["get"])
+@app.route("/movies",methods=["get"])
 def get_movieInfo():
-    movie=db.movieInfo.find_one({"title":"매트릭스"});   
-   
-    return jsonify({"title":movie["title"],"point":movie["point"]});
+    results=[]
+    movies=list(db.movieInfo.find());   
+    for movie in movies:
+        movie['_id'] = str(movie['_id'])
+        results.append(movie)
+        
+    return jsonify({"result":"success","movies":results});
 
 #데이터 입력
 @app.route("/movie",methods=["post"])
@@ -29,8 +57,13 @@ def input_movieInfo():
     if request.is_json:
         params = request.get_json()#해당 데이터 바인딩
         #print(params)
-        findMovieInfo(params["url"]);
-    return jsonify({"result":"success","resultCode":200});
+        returnObj = findMovieInfo(params);
+        returnObj['_id'] = str(returnObj['_id']);
+        if returnObj["url_image"] is not None:
+            return jsonify({"result":"success","resultCode":200,"article":returnObj});
+        else :
+            return jsonify({"result":"fail","resultCode":400});
+
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
 
